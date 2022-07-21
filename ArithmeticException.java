797f8d1ac5f0ede0b4956bb96fa7f61772cd369b
Namburi Naga jayasri <98807661+Jayasri1411@users.noreply.github.com>
2022-07-21 17:45:32 +0530

    Add files via upload

---
+++ ArithmeticException.java
@@ -0,0 +1,12 @@
+/* 1. Write a program which will illustrate ArthmeticException. Create a parameterized method which can take two values and print division of two numbers. 
+Please write your comments regarding ArithmeticException such as "When ArithmeticException will occur in this program?"
+*/
+public class ArithmeticException {
+
+    public static void main(String args[]) {
+      int num=10/0;
+      byte num1=50/0;
+      System.out.println(num);
+      System.out.println(num1);
+    }
+}
---
+++ ArrayIndexOutOfBoundsException.java
@@ -0,0 +1,22 @@
+/* 3. Write a program which will illustrate ArrayIndexOutOfBoundsException. 
+   Create an array variable with n size, accept values from user and store values into array, finally print all values on console. 
+   Please write your comments regarding ArrayIndexOutOfBoundsException such as " When ArrayIndexOutOfBoundsException occur in this program?"
+*/
+import java.util.Scanner;
+public class ArrayIndexOutOfBoundsException {
+
+    public static void main(String args[]) {
+     Scanner sc=new Scanner(System.in);
+      System.out.println("Enter array elements");
+      int arr[]=new int[5];
+      for(int i=0;i<=5;i++)
+        {
+          arr[i]=sc.nextInt();
+        }
+      for(int i=0;i<=5;i++)//trying to enter 6th element it is showing array out of box index exception.
+        {
+          System.out.println(arr[i]);
+        }
+    }
+}
+
